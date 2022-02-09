57cec36582dcda7693b10a8fb86f21bd03fe7946
Oleg Sedinkin <a@aw.wtf>
2022-02-09 23:37:19 +0500

    🚧

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# package-skeleton 
-![Packagist Version](https://img.shields.io/packagist/v/hyqo/?style=flat-square)
-![Packagist PHP Version Support](https://img.shields.io/packagist/php-v/hyqo/?style=flat-square)
-![GitHub Workflow Status](https://img.shields.io/github/workflow/status/hyqo//run-tests?style=flat-square)
+# php-iso/iso-639
+![Packagist Version](https://img.shields.io/packagist/v/php-iso/iso-639/?style=flat-square)
+![Packagist PHP Version Support](https://img.shields.io/packagist/php-v/php-iso/iso-639/?style=flat-square)
+![GitHub Workflow Status](https://img.shields.io/github/workflow/status/php-iso/iso-639/run-tests?style=flat-square)
 
+- ISO 639-1: two-letter codes
 ## Install
 
 ```sh
-composer require hyqo/
+composer require php-iso/iso-639
 ```
--- tests/ISO639_1Test.php
+++ tests/ISO639_1Test.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace PhpIso\ISO639;
+namespace PhpIso\ISO639\Test;
 
+use PhpIso\ISO639\ISO639_1;
 use PHPUnit\Framework\TestCase;
 
 class ISO639_1Test extends TestCase
@@ -20,25 +21,32 @@ class ISO639_1Test extends TestCase
 
     public function test_get_native_name()
     {
-        $this->assertEquals('English', ISO639_1::getISOName('en'));
-        $this->assertEquals('Русский', ISO639_1::getISOName('ru'));
+        $this->assertEquals('English', ISO639_1::getNativeName('en'));
+        $this->assertEquals('Русский', ISO639_1::getNativeName('ru'));
     }
 
     public function test_get_639_2T()
     {
-        $this->assertEquals('fas', ISO639_1::getISOName('en'));
-        $this->assertEquals('cym', ISO639_1::get639_2B('cy'));
+        $this->assertEquals('fas', ISO639_1::get639_2T('fa'));
+        $this->assertEquals('cym', ISO639_1::get639_2T('cy'));
     }
 
     public function test_get_639_2B()
     {
-        $this->assertEquals('per', ISO639_1::getISOName('en'));
+        $this->assertEquals('per', ISO639_1::get639_2B('fa'));
         $this->assertEquals('wel', ISO639_1::get639_2B('cy'));
     }
 
     public function test_get_639_3()
     {
-        $this->assertEquals('fas', ISO639_1::getISOName('en'));
-        $this->assertEquals('cym', ISO639_1::get639_2B('cy'));
+        $this->assertEquals('fas', ISO639_1::get639_3('fa'));
+        $this->assertEquals('cym', ISO639_1::get639_3('cy'));
+    }
+
+    public function test_codes()
+    {
+        foreach (ISO639_1::CODES as $code => $values) {
+            $this->assertEquals(5, count($values), $code);
+        }
     }
 }
